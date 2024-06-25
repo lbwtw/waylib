c2216ce5e46691f32249c47f16b9a4515691ec31
lbwtw <xiaoyaobing@uniontech.com>
2024-06-25 16:44:40 +0800

    add input device config
    Issue: https://github.com/vioken/waylib/issues/384

--- src/server/kernel/winputdevice.cpp
+++ src/server/kernel/winputdevice.cpp
@@ -6,10 +6,12 @@
 #include "private/wglobal_p.h"
 
 #include <qwinputdevice.h>
+#include <qwbackend.h>
 
 #include <QDebug>
 #include <QInputDevice>
 #include <QPointer>
+#include <QLoggingCategory>
 
 extern "C" {
 #include <wlr/types/wlr_input_device.h>
@@ -18,6 +20,8 @@ extern "C" {
 
 QW_USE_NAMESPACE
 WAYLIB_SERVER_BEGIN_NAMESPACE
+
+Q_LOGGING_CATEGORY(qLcWInputDevice, "waylib.server.inputdevice", QtWarningMsg)
 
 class WInputDevicePrivate : public WWrapObjectPrivate
 {
@@ -107,6 +111,377 @@ QInputDevice *WInputDevice::qtDevice() const
 {
     W_DC(WInputDevice);
     return d->qtDevice;
+}
+
+libinput_device *WInputDevice::libinput_device_handle()
+{
+    return QWLibinputBackend::getDeviceHandle(handle());
+}
+
+bool WInputDevice::setSendEventsMode(uint32_t mode)
+{
+    return WInputDevice::configSendEventsMode(libinput_device_handle(), mode);
+}
+
+bool WInputDevice::setTapButtonMap(libinput_config_tap_button_map map)
+{
+    return WInputDevice::configTapButtonMap(libinput_device_handle(), map);
+}
+
+bool WInputDevice::setTapEnabled(libinput_config_tap_state tap)
+{
+    return WInputDevice::configTapEnabled(libinput_device_handle(), tap);
+}
+
+bool WInputDevice::setTapDragEnabled(libinput_config_drag_state drag)
+{
+    return WInputDevice::configTapDragEnabled(libinput_device_handle(), drag);
+}
+
+bool WInputDevice::setTapDragLock(libinput_config_drag_lock_state lock)
+{
+    return WInputDevice::configTapDragLockEnabled(libinput_device_handle(), lock);
+}
+
+bool WInputDevice::setAccelSpeed(qreal speed)
+{
+    return WInputDevice::configAccelSpeed(libinput_device_handle(), speed);
+}
+
+bool WInputDevice::setRotationAngle(qreal angle)
+{
+    return WInputDevice::configRotationAngle(libinput_device_handle(), angle);
+}
+
+bool WInputDevice::setAccelProfile(libinput_config_accel_profile profile)
+{
+    return WInputDevice::configAccelProfile(libinput_device_handle(), profile);
+}
+
+bool WInputDevice::setNaturalScroll(bool natural)
+{
+    return WInputDevice::configNaturalScroll(libinput_device_handle(), natural);
+}
+
+bool WInputDevice::setLeftHanded(bool left)
+{
+    return WInputDevice::configLeftHanded(libinput_device_handle(), left);
+}
+
+bool WInputDevice::setClickMethod(libinput_config_click_method method)
+{
+    return WInputDevice::configClickMethod(libinput_device_handle(), method);
+}
+
+bool WInputDevice::setMiddleEmulation(libinput_config_middle_emulation_state mid)
+{
+    return WInputDevice::configMiddleEmulation(libinput_device_handle(), mid);
+}
+
+bool WInputDevice::setScrollMethod(libinput_config_scroll_method method)
+{
+    return WInputDevice::configScrollMethod(libinput_device_handle(), method);
+}
+
+bool WInputDevice::setScrollButton(uint32_t button)
+{
+    return WInputDevice::configScrollButton(libinput_device_handle(), button);
+}
+
+bool WInputDevice::setScrollButtonLock(libinput_config_scroll_button_lock_state lock)
+{
+    return WInputDevice::configScrollButtonLock(libinput_device_handle(), lock);
+}
+
+bool WInputDevice::setDwt(libinput_config_dwt_state enable)
+{
+    return WInputDevice::configDwtEnabled(libinput_device_handle(), enable);
+}
+
+bool WInputDevice::setDwtp(libinput_config_dwtp_state enable)
+{
+    return WInputDevice::configDwtpEnabled(libinput_device_handle(), enable);
+}
+
+bool WInputDevice::setCalibrationMatrix(float mat[])
+{
+    return WInputDevice::configCalibrationMatrix(libinput_device_handle(), mat);
+}
+
+bool WInputDevice::ensureStatus(libinput_config_status status)
+{
+    if (status != LIBINPUT_CONFIG_STATUS_SUCCESS) {
+        qCCritical(qLcWInputDevice) << "Failed to apply libinput config: "
+                                    << libinput_config_status_to_str(status);
+        return false;
+    }
+
+    return true;
+}
+
+bool WInputDevice::configSendEventsMode(libinput_device *device, uint32_t mode)
+{
+    if (libinput_device_config_send_events_get_mode(device) == mode) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_send_events_set_mode repeat set mode" << mode;
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_send_events_set_mode " << mode;
+    enum libinput_config_status status = libinput_device_config_send_events_set_mode(device, mode);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configTapEnabled(libinput_device *device, libinput_config_tap_state tap)
+{
+    if (libinput_device_config_tap_get_finger_count(device) <= 0 ||
+        libinput_device_config_tap_get_enabled(device) == tap) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_tap_set_enabled: " << tap <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_tap_set_enabled: " << tap;
+    enum libinput_config_status status = libinput_device_config_tap_set_enabled(device, tap);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configTapButtonMap(libinput_device *device, libinput_config_tap_button_map map)
+{
+    if (libinput_device_config_tap_get_finger_count(device) <= 0 ||
+        libinput_device_config_tap_get_button_map(device) == map) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_tap_set_button_map: " << map <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_tap_set_button_map: " << map;
+    enum libinput_config_status status = libinput_device_config_tap_set_button_map(device, map);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configTapDragEnabled(libinput_device *device, libinput_config_drag_state drag)
+{
+    if (libinput_device_config_tap_get_finger_count(device) <= 0 ||
+        libinput_device_config_tap_get_drag_enabled(device) == drag) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_tap_set_drag_enabled: " << drag <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_tap_set_drag_enabled: " << drag;
+    enum libinput_config_status status = libinput_device_config_tap_set_drag_enabled(device, drag);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configTapDragLockEnabled(libinput_device *device, libinput_config_drag_lock_state lock)
+{
+    if (libinput_device_config_tap_get_finger_count(device) <= 0 ||
+        libinput_device_config_tap_get_drag_lock_enabled(device) == lock) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_tap_set_drag_enabled: " << lock <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_tap_set_drag_lock_enabled: " << lock;
+    enum libinput_config_status status = libinput_device_config_tap_set_drag_lock_enabled(device, lock);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configAccelSpeed(libinput_device *device, double speed)
+{
+    if (!libinput_device_config_accel_is_available(device) ||
+        libinput_device_config_accel_get_speed(device) == speed) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_accel_set_speed: " << speed <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_accel_set_speed: " << speed;
+    enum libinput_config_status status = libinput_device_config_accel_set_speed(device, speed);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configRotationAngle(libinput_device *device, double angle)
+{
+    if (!libinput_device_config_rotation_is_available(device) ||
+        libinput_device_config_rotation_get_angle(device) == angle) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_rotation_set_angle: " << angle <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_rotation_set_angle: " << angle;
+    enum libinput_config_status status = libinput_device_config_rotation_set_angle(device, angle);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configAccelProfile(libinput_device *device, libinput_config_accel_profile profile)
+{
+    if (!libinput_device_config_accel_is_available(device) ||
+        libinput_device_config_accel_get_profile(device) == profile) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_accel_set_profile: " << profile <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_accel_set_profile: " << profile;
+    enum libinput_config_status status = libinput_device_config_accel_set_profile(device, profile);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configNaturalScroll(libinput_device *device, bool natural)
+{
+    if (!libinput_device_config_scroll_has_natural_scroll(device) ||
+        libinput_device_config_scroll_get_natural_scroll_enabled(device) == natural) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_scroll_set_natural_scroll_enabled: " << natural <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_scroll_set_natural_scroll_enabled: " << natural;
+    enum libinput_config_status status = libinput_device_config_scroll_set_natural_scroll_enabled(device, natural);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configLeftHanded(libinput_device *device, bool left)
+{
+    if (!libinput_device_config_left_handed_is_available(device) ||
+        libinput_device_config_left_handed_get(device) == left) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_left_handed_set: " << left <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_left_handed_set: " << left;
+    enum libinput_config_status status = libinput_device_config_left_handed_set(device, left);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configClickMethod(libinput_device *device, libinput_config_click_method method)
+{
+    uint32_t click = libinput_device_config_click_get_methods(device);
+    if ((click & ~LIBINPUT_CONFIG_CLICK_METHOD_NONE) == 0 ||
+        libinput_device_config_click_get_method(device) == method) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_click_set_method: " << method <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_click_set_method: " << method;
+    enum libinput_config_status status = libinput_device_config_click_set_method(device, method);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configMiddleEmulation(libinput_device *device, libinput_config_middle_emulation_state mid)
+{
+    if (!libinput_device_config_middle_emulation_is_available(device) ||
+        libinput_device_config_middle_emulation_get_enabled(device) == mid) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_middle_emulation_set_enabled: " << mid <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_middle_emulation_set_enabled: " << mid;
+    enum libinput_config_status status = libinput_device_config_middle_emulation_set_enabled(device, mid);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configScrollMethod(libinput_device *device, libinput_config_scroll_method method)
+{
+    uint32_t scroll = libinput_device_config_scroll_get_methods(device);
+    if ((scroll & ~LIBINPUT_CONFIG_SCROLL_NO_SCROLL) == 0 ||
+        libinput_device_config_scroll_get_method(device) == method) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_scroll_set_method: " << method <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_scroll_set_method: " << method;
+    enum libinput_config_status status = libinput_device_config_scroll_set_method(device, method);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configScrollButton(libinput_device *device, uint32_t button)
+{
+    uint32_t scroll = libinput_device_config_scroll_get_methods(device);
+    if ((scroll & ~LIBINPUT_CONFIG_SCROLL_NO_SCROLL) == 0 ||
+        libinput_device_config_scroll_get_button(device) == button) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_scroll_set_button: " << button <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_scroll_set_button: " << button;
+    enum libinput_config_status status = libinput_device_config_scroll_set_button(device, button);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configScrollButtonLock(libinput_device *device, libinput_config_scroll_button_lock_state lock)
+{
+    uint32_t scroll = libinput_device_config_scroll_get_methods(device);
+    if ((scroll & ~LIBINPUT_CONFIG_SCROLL_NO_SCROLL) == 0 ||
+        libinput_device_config_scroll_get_button_lock(device) == lock) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_scroll_set_button_lock: " << lock <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_scroll_set_button: " << lock;
+    enum libinput_config_status status = libinput_device_config_scroll_set_button_lock(device, lock);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configDwtEnabled(libinput_device *device, enum libinput_config_dwt_state enable)
+{
+    if (!libinput_device_config_dwt_is_available(device) ||
+        libinput_device_config_dwt_get_enabled(device) == enable) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_dwt_set_enabled: " << enable <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_dwt_set_enabled: " << enable;
+    enum libinput_config_status status = libinput_device_config_dwt_set_enabled(device, enable);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configDwtpEnabled(libinput_device *device, enum libinput_config_dwtp_state enable)
+{
+    if (!libinput_device_config_dwtp_is_available(device) ||
+        libinput_device_config_dwtp_get_enabled(device) == enable) {
+        qCCritical(qLcWInputDevice) << "libinput_device_config_dwtp_set_enabled: " << enable <<" is invalid";
+        return false;
+    }
+
+    qCDebug(qLcWInputDevice) << "libinput_device_config_dwt_set_enabled: " << enable;
+    enum libinput_config_status status = libinput_device_config_dwtp_set_enabled(device, enable);
+
+    return ensureStatus(status);
+}
+
+bool WInputDevice::configCalibrationMatrix(libinput_device *device, float mat[])
+{
+    if (!libinput_device_config_calibration_has_matrix(device)) {
+        qCCritical(qLcWInputDevice) << "setCalibrationMatrix mat is invalid";
+        return false;
+    }
+    bool changed = false;
+    float current[6];
+    libinput_device_config_calibration_get_matrix(device, current);
+    enum libinput_config_status status = LIBINPUT_CONFIG_STATUS_UNSUPPORTED;
+    for (int i = 0; i < 6; i++) {
+        if (current[i] != mat[i]) {
+            changed = true;
+            break;
+        }
+    }
+    if (changed) {
+        qCDebug(qLcWInputDevice, "libinput_device_config_calibration_set_matrix(%f, %f, %f, %f, %f, %f)",
+                mat[0], mat[1], mat[2], mat[3], mat[4], mat[5]);
+        status = libinput_device_config_calibration_set_matrix(device, mat);
+    }
+    return changed && ensureStatus(status);
 }
 
 WAYLIB_SERVER_END_NAMESPACE
--- src/server/kernel/winputdevice.h
+++ src/server/kernel/winputdevice.h
@@ -7,6 +7,8 @@
 #include <QObject>
 #include <qwglobal.h>
 
+#include <libinput.h>
+
 QW_BEGIN_NAMESPACE
 class QWInputDevice;
 QW_END_NAMESPACE
@@ -50,6 +52,57 @@ public:
     Type type() const;
     void setSeat(WSeat *seat);
     WSeat *seat() const;
+
+    libinput_device *libinput_device_handle();
+
+    bool setSendEventsMode(uint32_t mode);
+    bool setTapEnabled(enum libinput_config_tap_state tap);
+    bool setTapButtonMap(enum libinput_config_tap_button_map map);
+    bool setTapDragEnabled(enum libinput_config_drag_state drag);
+    bool setTapDragLock(enum libinput_config_drag_lock_state lock);
+    bool setAccelSpeed(qreal speed);
+    bool setRotationAngle(qreal angle);
+    bool setAccelProfile(enum libinput_config_accel_profile profile);
+    bool setNaturalScroll(bool natural);
+    bool setLeftHanded(bool left);
+    bool setClickMethod(enum libinput_config_click_method method);
+    bool setMiddleEmulation(enum libinput_config_middle_emulation_state mid);
+    bool setScrollMethod(enum libinput_config_scroll_method method);
+    bool setScrollButton(uint32_t button);
+    bool setScrollButtonLock(enum libinput_config_scroll_button_lock_state lock);
+    bool setDwt(enum libinput_config_dwt_state enable);
+    bool setDwtp(enum libinput_config_dwtp_state enable);
+    bool setCalibrationMatrix(float mat[6]);
+
+    static bool ensureStatus(enum libinput_config_status status);
+
+    static bool configSendEventsMode(struct libinput_device *device, uint32_t mode);
+    static bool configTapEnabled(struct libinput_device *device,
+                       enum libinput_config_tap_state tap);
+    static bool configTapButtonMap(struct libinput_device *device,
+                                   enum libinput_config_tap_button_map map);
+    static bool configTapDragEnabled(struct libinput_device *device,
+                             enum libinput_config_drag_state drag);
+    static bool configTapDragLockEnabled(struct libinput_device *device,
+                                  enum libinput_config_drag_lock_state lock);
+    static bool configAccelSpeed(struct libinput_device *device, double speed);
+    static bool configRotationAngle(struct libinput_device *device, double angle);
+    static bool configAccelProfile(struct libinput_device *device,
+                                enum libinput_config_accel_profile profile);
+    static bool configNaturalScroll(struct libinput_device *device, bool natural);
+    static bool configLeftHanded(struct libinput_device *device, bool left);
+    static bool configClickMethod(struct libinput_device *device,
+                                 enum libinput_config_click_method method);
+    static bool configMiddleEmulation(struct libinput_device *device,
+                                     enum libinput_config_middle_emulation_state mid);
+    static bool configScrollMethod(struct libinput_device *device,
+                                enum libinput_config_scroll_method method);
+    static bool configScrollButton(struct libinput_device *device, uint32_t button);
+    static bool configScrollButtonLock(struct libinput_device *device,
+                                       enum libinput_config_scroll_button_lock_state lock);
+    static bool configDwtEnabled(struct libinput_device *device, enum libinput_config_dwt_state enable);
+    static bool configDwtpEnabled(struct libinput_device *device, enum libinput_config_dwtp_state enable);
+    static bool configCalibrationMatrix(struct libinput_device *device, float mat[6]);
 
 private:
     friend class QWlrootsIntegration;
